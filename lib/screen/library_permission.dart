8bf08344925182d7cfc8c34dd683907c7dc3154e
james.jeon <no@email.com>
2021-08-12 17:39:12 -0500

    getting permission finished

--- lib/screen/library_permission.dart
+++ lib/screen/library_permission.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 import 'auth/sign_up.dart';
 
@@ -6,16 +7,110 @@ class LibraryPermissionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Center(
-        child: CupertinoButton(
-            child: Text("library permission"),
-            onPressed: () {
-              Navigator.of(context)
-                  .pushReplacement(CupertinoPageRoute(builder: (context) {
-                return SignUp();
-              }));
-            }),
-      ),
+      backgroundColor: Color.fromRGBO(256, 223, 77, 1),
+      child: LibraryPermissionBody(),
     );
   }
+}
+
+class LibraryPermissionBody extends StatefulWidget {
+  const LibraryPermissionBody({Key? key}) : super(key: key);
+
+  @override
+  _LibraryPermissionBodyState createState() => _LibraryPermissionBodyState();
+}
+
+class _LibraryPermissionBodyState extends State<LibraryPermissionBody> {
+  bool permission = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        top: true,
+        bottom: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Icon(
+                CupertinoIcons.photo,
+                color: CupertinoColors.black,
+                size: 150,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12, bottom: 30),
+              child: Text(
+                "Cloud Photos need full access to your media library",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12, right: 12, bottom: 25),
+              child: Text(
+                "Cloud Photos app need permission to let you view and backup photos from this device",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Visibility(
+              visible: !permission,
+              child: CupertinoButton(
+                  child: Text("Allow Access to all photos"),
+                  onPressed: () async {
+                    var result = await PhotoManager.requestPermissionExtend();
+                    if (result == PermissionState.authorized) {
+                      Navigator.of(context).pushReplacement(
+                          CupertinoPageRoute(builder: (context) {
+                        return SignUp();
+                      }));
+                    } else {
+                      setState(() {
+                        permission = !permission;
+                        print(permission);
+                      });
+                    }
+                  }),
+            ),
+            Visibility(
+              child: Column(
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 12, right: 12, bottom: 20),
+                    child: Text(
+                      "Cloud Photos Don't have permission on this device, Please allow full access to photos in settings",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CupertinoColors.systemRed),
+                    ),
+                  ),
+                  CupertinoButton(
+                      child: Text("Open Setting"),
+                      onPressed: () {
+                        PhotoManager.openSetting();
+                      }),
+                ],
+              ),
+              visible: permission,
+            )
+          ],
+        ));
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return GestureDetector(
+  //     onTap: () {
+  //       setState(() {
+  //         count++;
+  //         print(count);
+  //       });
+  //     },
+  //     child: Center(child: Text("$count")),
+  //   );
+  // }
 }
