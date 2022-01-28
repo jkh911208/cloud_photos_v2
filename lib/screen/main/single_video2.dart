73810effbc613a672ec4d89741089d67ebcbdb0e
James Jeon <jkh911208@gmail.com>
2022-01-28 10:53:34 -0600

    flutter version up

---
+++ lib/screen/main/single_video2.dart
@@ -0,0 +1,27 @@
+// import 'dart:io';
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:better_player/better_player.dart';
+
+// class SingleVideo2 extends StatefulWidget {
+//   final File file;
+//   final PageController pageController;
+//   final int currentPage;
+
+//   const SingleVideo2(
+//       {Key? key,
+//       required this.file,
+//       required this.pageController,
+//       required this.currentPage})
+//       : super(key: key);
+
+//   @override
+//   _SingleVideo2State createState() => _SingleVideo2State();
+// }
+
+// class _SingleVideo2State extends State<SingleVideo2> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return BetterPlayer.file(widget.file.path);
+//   }
+// }
